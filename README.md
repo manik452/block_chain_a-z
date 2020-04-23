ce7d31323dc2e8d2d070ee4573126103e6e7a638
manik452 <manik452@yahoo.com>
2020-04-23 23:34:00 +0600

    solidity implementation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # block_chain_a-z
+#create Smartcontact
+######first of all download ganache
+https://www.trufflesuite.com/ganache 
+######download eatherwallet below link
+https://github.com/MyEtherWallet/etherwallet/projects
+
+https://remix.ethereum.org/#optimize=false&evmVersion=null&version=soljson-v0.4.26+commit.4563c3fc.js
---
+++ module-3_create_smartcontact/hadcoin_ico.sol
@@ -0,0 +1,54 @@
+//Hadcoins ICO
+
+//Version of compiler
+pragma solidity ^0.4.11;
+
+contract hadcoin_ico {
+// Intraducing the maximum number of hadcoins available for sale
+uint public max_hadcoins = 1000000;
+
+//Introducing the USD to hadcoins conversion rate
+uint public usd_to_hadcoins = 1000;
+
+//Introducing the to tal number of Hadcoins that have been bought by
+//the investors
+uint public total_hadcoins_bought = 0;
+
+//Mapping from the investor address to its equity in Hadcoins and USD
+mapping(address => uint) equity_hadcoins;
+mapping(address => uint) equity_usd;
+
+//Checking if an investor can but Hadcoins
+modifier can_buy_hadcoins(uint usd_invested) {
+require (usd_invested * usd_to_hadcoins + total_hadcoins_bought <= max_hadcoins);
+_;
+}
+
+//Getting the equity in Hadcoins of an investor
+function equity_in_hadcoins(address investor) external constant returns (uint){
+return equity_hadcoins[investor];
+}
+
+//Getting the equity in USD of an investor
+function equity_in_hadcoins(address investor) external constant returns (uint){
+return equity_usd[investor];
+}
+
+//Buying Hadcoins
+function by_hadcoins(address investor, uint usd_invested) external
+can_buy_hadcoins(usd_invested){
+uint hadcoins_bought  = usd_invested * usd_to_hadcoins;
+equity_hadcoins[investor] += hadcoins_bought;
+equity_usd[investor] = equity_hadcoins[investor] / 1000;
+total_hadcoins_bought += hadcoins_bought;
+}
+
+
+//Selling Hadcoins
+function sell_hadcoins(address investor, uint hadcoins_sold){
+equity_hadcoins[investor] -= hadcoins_sold;
+equity_usd[investor] = equity_hadcoins[investor] / 1000;
+total_hadcoins_bought -= hadcoins_sold;
+}
+
+}
